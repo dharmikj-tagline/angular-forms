8189fdf5f523eeb481739f75443d234e00640134
dharmikj <dharmikj.tagline@gmail.com>
2023-01-12 10:39:59 +0530

    Implement checkbox required

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, EventEmitter, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormArray,
   FormBuilder,
@@ -6,7 +6,13 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { CityList, CountryList, StateFinds, StateList, UserData } from '../common';
+import {
+  CityList,
+  CountryList,
+  StateFinds,
+  StateList,
+  UserData,
+} from '../common';
 
 @Component({
   selector: 'app-reactive',
@@ -30,10 +36,7 @@ export class ReactiveComponent implements OnInit {
   lblEmail: string = 'Email : ';
   lblMobile: string = 'Mobile : ';
   lblSubmit: string = 'Submit';
-  formErr: string = '';
   notHobbies: string = 'Not Applicable';
-  // states!: string[];
-  // cities!: string[];
   states!: any;
   cities!: any;
   userId!: number;
@@ -68,10 +71,11 @@ export class ReactiveComponent implements OnInit {
       mobile: '1234567890',
       date: '2003-02-25',
       hobbies: ['Playing', 'Watchig Movies'],
+      checkbox: true,
       address: {
         country: 'India',
-        state: 'Gujrat',
-        city: 'Surat',
+        state: 'Gujarat',
+        city: 'Ahmedabad',
       },
     },
     {
@@ -83,6 +87,7 @@ export class ReactiveComponent implements OnInit {
       mobile: '8765467890',
       date: '2020-01-11',
       hobbies: ['Playing', 'Reading'],
+      checkbox: true,
       address: {
         country: 'USA',
         state: 'New York',
@@ -90,105 +95,6 @@ export class ReactiveComponent implements OnInit {
       },
     },
   ];
-
-  // reactiveForm = new FormGroup({
-  //   fname: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern('^[a-zA-Z]+$'),
-  //     Validators.minLength(2),
-  //   ]),
-  //   lname: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern('^[a-zA-Z]+$'),
-  //     Validators.minLength(2),
-  //   ]),
-  //   gender: new FormControl('', [Validators.required]),
-  //   // address: new FormGroup({
-  //   //   country: new FormControl('' ),
-  //   //   state: new FormControl('' ),
-  //   //   city: new FormControl(''),
-  //   // }),
-  //   email: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
-  //   ]),
-  //   mobile: new FormControl('', [
-  //     Validators.required,
-  //     Validators.pattern('^[0-9]+$'),
-  //     Validators.minLength(10),
-  //     Validators.maxLength(10),
-  //   ]),
-  // });
-
-  // countryList: any = [
-  //   {
-  //     name: 'India',
-  //     state: ['Gujrat', 'Maharashtra', 'Kolkata', 'Banglore', 'Delhi'],
-  //   },
-  //   {
-  //     name: 'Germany',
-  //     state: ['Duesseldorf', 'Leinfelden-Echterdingen', 'Eschborn'],
-  //   },
-  //   {
-  //     name: 'Spain',
-  //     state: ['Barcelona', 'Madrid'],
-  //   },
-  //   {
-  //     name: 'USA',
-  //     state: ['Downers Grove', 'New York'],
-  //   },
-  // ];
-
-  // stateFinds: any = [
-  //   {
-  //     name: 'Gujrat',
-  //     city: ['Surat', 'Rajkot', 'Ahmdabad'],
-  //   },
-  //   {
-  //     name: 'Maharashtra',
-  //     city: ['Mumbai', 'Thana'],
-  //   },
-  //   {
-  //     name: 'Kolkata',
-  //     city: ['KolkataCity1', 'KolkataCity2'],
-  //   },
-  //   {
-  //     name: 'Banglore',
-  //     city: ['Bang', 'Luru'],
-  //   },
-  //   {
-  //     name: 'Delhi',
-  //     city: ['New Delhi', 'Old Delhi'],
-  //   },
-  //   {
-  //     name: 'Duesseldorf',
-  //     city: ['Duessel', 'dorf'],
-  //   },
-  //   {
-  //     name: 'Leinfelden-Echterdingen',
-  //     city: ['Leinfelden', 'Echterdingen'],
-  //   },
-  //   {
-  //     name: 'Eschborn',
-  //     city: ['Esch', 'born'],
-  //   },
-  //   {
-  //     name: 'Barcelona',
-  //     city: ['Barce', 'lona'],
-  //   },
-  //   {
-  //     name: 'Madrid',
-  //     city: ['MadCity1', 'MadCity2'],
-  //   },
-  //   {
-  //     name: 'Downers Grove',
-  //     city: ['Downers', 'Grove'],
-  //   },
-  //   {
-  //     name: 'New York',
-  //     city: ['New', 'York'],
-  //   },
-  // ];
 
   countryList: any = [
     { id: 1, name: 'India' },
@@ -223,15 +129,15 @@ export class ReactiveComponent implements OnInit {
 
   showData(): void {
     const address = this.reactiveForm.value.address;
-        const currentCountry = this.countryList.find(
-          (country: CountryList) => country.id == address.country
-        ).name;
-        const currentState = this.stateList.find(
-          (state: StateList) => state.id == address.state
-        ).name;
-        const currentCity = this.cityList.find(
-          (cit: CityList) => cit.id == address.city
-        ).name;
+    const currentCountry = this.countryList.find(
+      (country: CountryList) => country.id == address.country
+    ).name;
+    const currentState = this.stateList.find(
+      (state: StateList) => state.id == address.state
+    ).name;
+    const currentCity = this.cityList.find(
+      (cit: CityList) => cit.id == address.city
+    ).name;
 
     if (this.reactiveForm.invalid) {
       return;
@@ -250,12 +156,7 @@ export class ReactiveComponent implements OnInit {
             city: currentCity,
           },
         };
-        console.log('Updated Data ', this.userDatas);
       } else {
-        
-        // console.log('selectCountry :>> ', currentCountry);
-        // console.log('selectState :>> ', currentState);
-        // console.log('selectCity :>> ', currentCity);
 
         const data = {
           id: this.userDatas.length + 1,
@@ -273,7 +174,7 @@ export class ReactiveComponent implements OnInit {
         this.userDatas.push(data);
         console.log('this.userDatas ', this.userDatas);
       }
-      console.log(this.userDatas);
+
       this.userId = 0;
       this.reactiveForm.reset();
       console.log('this.reactiveForm.value :>> ', this.userDatas);
@@ -303,18 +204,6 @@ export class ReactiveComponent implements OnInit {
     return this.reactiveForm.controls;
   }
 
-  get fname() {
-    return this.reactiveForm.get('fname');
-  }
-
-  get lname() {
-    return this.reactiveForm.get('lname');
-  }
-
-  get gender() {
-    return this.reactiveForm.get('gender');
-  }
-
   get country() {
     return this.reactiveForm.get('address.country');
   }
@@ -327,47 +216,9 @@ export class ReactiveComponent implements OnInit {
     return this.reactiveForm.get('address.city');
   }
 
-  get date() {
-    return this.reactiveForm.get('date');
-  }
-
-  // get hobbies() {
-  //   return this.reactiveForm.get('hobbies') as FormArray;
-  // }
-
-  get email() {
-    return this.reactiveForm.get('email');
-  }
-
-  get mobile() {
-    return this.reactiveForm.get('mobile');
-  }
-
-  get checkbox() {
-    return this.reactiveForm.get('checkbox');
-  }
-
-  // changeCountry(count : any) : void {
-  //   console.log(count.target.value);
-  //   this.states = this.countryList.find(
-  //     (con: CountryList) => con.name == count.target.value
-  //   ).state;
-
-  //   console.log('States data', this.states);
-  // }
-
-  // changeState(count: any) :void {
-  //   console.log(count.target.value);
-  //   this.cities = this.stateFinds.find(
-  //     (con: StateFinds) => con.name == count.target.value
-  //   ).city;
-
-  //   console.log('Citys data', this.cities);
-  // }
-
   changeCountry(count: any) {
     console.log('count.target', count.target.value);
-    console.log(this.state);
+    console.log(this.states);
     this.states = this.stateList.filter(
       (con: StateList) => con.country == count.target.value
     );
@@ -382,43 +233,5 @@ export class ReactiveComponent implements OnInit {
     console.log(this.cities);
   }
 
-  hobbySelect(select: any) {
-    console.log('hoobies event', select);
-    let hobbyArr = this.reactiveForm.get('hobbies') as FormArray;
-
-    if (select.target.checked) {
-      hobbyArr.push(new FormControl(select.target.value));
-      console.log('Hobbies Value : ', hobbyArr.value);
-      console.log('Hobbies length : ', hobbyArr.length);
-    } else {
-      let i = 0;
-      hobbyArr.controls.forEach((hobby: any) => {
-        console.log('hobby object value', hobby.value);
-        if (hobby.value == select.target.value) {
-          hobbyArr.removeAt(i);
-        }
-        i++;
-      });
-      if(hobbyArr.length == 0){
-          hobbyArr.push(new FormControl(this.notHobbies));
-        }
-    }
-  }
+  
 }
-
-// if(select.target.checked == false){
-//   hobbyArr.push(new FormControl(this.notHobbies));
-// }
-
-// if(hobbyArr.length == 0){
-//   hobbyArr.push(new FormControl(this.notHobbies));
-// }
-
-// setDefaut() {
-//   this.fname?.setValue('Dharmik');
-//   this.lname?.setValue('Jikadra');
-//   this.gender?.setValue('male');
-//   this.email?.setValue('dharmik@gmail.com');
-//   this.mobile?.setValue('8899007766');
-//   this.country?.setValue('India');
-// }
